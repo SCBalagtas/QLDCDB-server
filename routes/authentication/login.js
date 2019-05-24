2f0a2ec2bb57e498b2dad351680c01768bdef2c6
Steven Balagtas <scbalagtas@gmail.com>
2019-05-24 10:48:20 +1000

    Implemented login endpoint with JWT

--- routes/authentication/login.js
+++ routes/authentication/login.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var router = express.Router();
 
+// declare JWT middleware
+const jwt = require('jsonwebtoken');
+
 /* POST email and password to check if user exists in the web_computing database. 
    If user exists, send back a JSON web token. */
 router.post('/', function(req, res, next) {
@@ -15,18 +18,29 @@ router.post('/', function(req, res, next) {
       'password': req.body.password
     };
     // check if the user exists in the database
-    req.db.from('users').select('email', 'password').where(user)
+    req.db.from('users').select('id','email', 'password').where(user)
     .then((row) => {
       // throw if no user is found
       if (row.length > 0) {
-        res.status(200).json({ message: 'Login successful' });
-        console.log('Successful login:', JSON.stringify(user)); // temporary logging
+        // create a payload for the jwt
+        const payload = {
+          'id': row[0].id,
+          'email': row[0].email
+        }
+        // generate and return jwt
+        jwt.sign({ user: payload }, 'secretkey', { expiresIn: 86400 }, (err, token) => {
+          res.status(200).json({ token: token, 
+                                 access_token: token, 
+                                 token_type: 'Bearer', 
+                                 expiresIn: 86400 });
+          console.log('Successful login:', JSON.stringify(user)); // temporary logging
+        });
       } else {
         throw new Error('Invalid login - incorrect email or password');
       }
 
-    }).catch(() => {
-      res.status(401).json({ message: 'Error: Invalid login - incorrect email or password' });
+    }).catch((err) => {
+      res.status(401).json({ message: err });
       console.log('Error: Invalid login - incorrect email or password', JSON.stringify(req.body)); // temporary logging
     })
   }
